812497b961b114df3f8c8750385a7dd10a996197
Khin yati Sett <khinyatisett5@gmail.com>
2021-12-15 15:38:08 +0630

    [main] fixed space format

--- tutorials/tutorial_01/index.php
+++ tutorials/tutorial_01/index.php
@@ -15,15 +15,15 @@
         echo "<tr>";
         for($col=1;$col<=8;$col++)
         {
-        $total=$row+$col;
-        if($total%2==0)
-        {
-        echo "<td height=50px width=50px bgcolor=#FFFFFF></td>";
-        }
-		else
-		{
-          echo "<td height=30px width=30px bgcolor=#000000></td>";
-        }
+            $total=$row+$col;
+                if ($total%2==0)
+                {
+                    echo "<td height=50px width=50px bgcolor=#FFFFFF></td>";
+                }
+		        else
+		        {
+                    echo "<td height=30px width=30px bgcolor=#000000></td>";
+                }
         }
     echo "</tr>";
     }
